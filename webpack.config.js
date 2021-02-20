1bafc276dd07054c9b267a434393a2c4fa08b44d
KopfSzmercen <uks9@o2.pl>
2021-02-20 20:33:16 +0100

    Second commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.ts',
   module: {
     rules: [
